47ae519741723213a7cb868006f8231417015089
Julian Nymark <j@juliannymark.com>
2021-07-31 16:17:42 +0200

    fix bug that wouldn't let you click away the patch notes on mobile hah!

--- src/patches.ts
+++ src/patches.ts
@@ -1,5 +1,6 @@
 import { data } from "./data";
 import { constructs } from "./constructs";
+import { registerTapHandlers } from "./tapHandler";
 
 const VERSION = "0.0.2";
 
@@ -65,16 +66,16 @@ const patchConstructs = (version: string, patchMessages: string[]) => {
 };
 
 const showPatchMessages = (messages: string[]) => {
-  var modal = document.getElementById('modal');
-  var closeButton = document.getElementById('modal-close');
-  const modalContent = document.getElementById('modal-content');
+  var modal = document.getElementById("modal");
+  var closeButton = document.getElementById("modal-close");
+  const modalContent = document.getElementById("modal-content");
 
   if (messages.length === 0) {
     return;
   }
 
   for (const message of messages) {
-    const p = document.createElement('p');
+    const p = document.createElement("p");
     p.innerHTML = message;
     modalContent.appendChild(p);
   }
@@ -84,16 +85,24 @@ const showPatchMessages = (messages: string[]) => {
   closeButton.onclick = function () {
     modal.style.display = "none";
   };
+  registerTapHandlers(closeButton, () => modal.style.display = "none");
 
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   };
+  registerTapHandlers(window, (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
 };
 
 function patchGeneric(version: string, patchMessages: string[]) {
   if (vLessThan(version, "0.0.2")) {
-    patchMessages.push('now more human readable numbers! (million, trillion... etc!) thanks to <a href="https://github.com/JsCommunity/human-format">https://github.com/JsCommunity/human-format</a>');
+    patchMessages.push(
+      'now more human readable numbers! (million, trillion... etc!) thanks to <a href="https://github.com/JsCommunity/human-format">https://github.com/JsCommunity/human-format</a>',
+    );
   }
 }
